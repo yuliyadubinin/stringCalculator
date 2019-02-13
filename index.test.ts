f6892e055f4fa1539f3e9c0b9564ad4d55369697
Yuliya Dubinin <yuliyadubinin@gmail.com>
2019-02-13 14:42:41 +0100

    Add explicit separator test

--- index.test.ts
+++ index.test.ts
@@ -1,48 +1,58 @@
 import { StringCalculator } from "./index";
-describe("StringCalculator", function() {
+describe("StringCalculator", () => {
   describe("Add", function() {
-    test("return 0 for empty string", function() {
-      expect(StringCalculator.add('')).toBe(0);
+    test("Should return 0 for empty string", function() {
+      expect(StringCalculator.add("")).toBe(0);
     });
 
-    test("Should not allow negative numbers", function() {
-      expect(() => StringCalculator.add("-5")).toThrowError(
-        'No negative values are allowed: -5'
+    /*test("Should not allow negative numbers", function() {
+      expect(() => StringCalculator.add("-5, -3")).toThrowError(
+        "No negative values are allowed: -5, -3"
       );
-    });
+    });*/
 
-    test("Should return the number itself when a single number is passed", function() {
+    test("Should return the number itself", function() {
       expect(StringCalculator.add("7")).toBe(7);
     });
 
-    test("Should return the sum of the numbers if two are given", function() {
+    test("Should return the sum of two numbers", function() {
       expect(StringCalculator.add("7, 3")).toBe(10);
     });
 
-    test("Should return the sum of the numbers if two are given", function() {
-      expect(StringCalculator.add("7\n7")).toBe(14);
+    test("Should return the sum of an unknown amount of numbers", function() {
+      expect(StringCalculator.add("10,10,30,1,5")).toBe(56);
     });
 
     test("Numbers bigger than 1000 should be ignored", function() {
       expect(StringCalculator.add("10000, 3")).toBe(3);
     });
 
-    
-    test("Should allow \n or , in between the input number string", function() {
-      expect(StringCalculator.add("1\n23,1")).toBe(25);
+    test("Should allow \n in between the input number string", function() {
+      expect(StringCalculator.add("7\n7")).toBe(14);
     });
 
-    test("Should allow single delimiter in between the input number string", function() {
-      expect(StringCalculator.add("//;\n1;9;5")).toBe(15);
+    test("Should allow \n in between the input number string", function() {
+      expect(StringCalculator.add("7\n7,3")).toBe(17);
+    });
+
+    test("Should allow \n in between the input number string", function() {
+      expect(StringCalculator.add("7\n7,3,5")).toBe(22);
+    });
+
+    test("Should allow \n or , in between the input number string", function() {
+      expect(StringCalculator.add("1\n23,1,5")).toBe(30);
+    });
+
+    test("Explicit separator", function() {
+      expect(StringCalculator.add("//;\n1;9")).toBe(10);
     });
 
     test("Should allow two delimiters in between the input number string", function() {
-      expect(StringCalculator.add("//[??]10??20")).toBe(30);
+      expect(StringCalculator.add("//[??]15??20")).toBe(35);
     });
 
-
-    test("Should return the sum of an unknown amount of numbers", function() {
-      expect(StringCalculator.add("10,10,30,1,5")).toBe(56);
+    test("Should allow two delimiters in between the input number string", function() {
+      expect(StringCalculator.add("//[**]10**100")).toBe(110);
     });
   });
 });
