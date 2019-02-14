84511f7e29ccac63cd39b7c490d6664dd219c2a6
Yuliya Dubinin <yuliyadubinin@gmail.com>
2019-02-14 13:03:06 +0100

    Fix calcSum methode, fix finding multi char  delimiters

--- index.ts
+++ index.ts
@@ -1,86 +1,101 @@
 export class StringCalculator {
-  static add(inputString: string): number | undefined {
-    var separator = /,|\n/;
-    var delim = "";
+  static add(inputString: string): number {
+    var processedDelimStr = inputString;
+
+    var staticCommaDelim = ","; // static delimiter "," and "\n" (always should be used)
+    var staticNewLineDelim = "\n"; // static delimiter "\n" (always should be used)
+    var dynamicSingleDelim = ""; //single delim; dynamic (optional) delimiter, holds SINGLE delimiter char (if exists)
+    var dynamicFirstMultiDelim = ""; //first multi delim; dynamic (optional) delimiter, holds FIRST multiple delimiter chars between [ ] (if exists)
+    var dynamicSecMultiDelim = ""; //second multi delim; dynamic (optional) delimiter, holds SECOND multiple delimiter chars between [ ] (if exists)
+
+    var arrayNumbersForSum = []; // holds "numbers" in string representation
+    var arrayNumbers = [];
 
     var startOfDelim = "//";
     var startOfMultiDelim = "[";
     var endOfMultiDelim = "]";
-    var tempEndStringIndex = 0;
-    var arrayOfNumbers = [];
+    var tempEndStrIndex = 0;
 
-    // delimiters that start from "//"
-    if (inputString.indexOf(startOfDelim) === 0) {
-      inputString = inputString.slice(2); // remove startOfDelim //
-      if (inputString.indexOf(startOfMultiDelim) === 0) {
-        inputString = inputString.slice(1); // remove first char from left (startOfMultiDelim)
-        tempEndStringIndex = inputString.indexOf(endOfMultiDelim);
-        const rest = inputString.slice(3);
-        delim = inputString.slice(0, 1); // assigning first char after removed //
-        const array = rest.split(delim).filter(Boolean);
+    // process single and multi-char delimiters that start from "//"
+    if (processedDelimStr.indexOf(startOfDelim) === 0) {
+      processedDelimStr = processedDelimStr.slice(2); // remove startOfDelim //
 
-        // convert array of strings to array of numbers
-        for (var i = 0; i < array.length; i++) {
-          arrayOfNumbers[i] = Number(array[i]);
+      // finding multi char delimiter(s)
+      if (processedDelimStr.indexOf(startOfMultiDelim) === 0) {
+        processedDelimStr = processedDelimStr.slice(1); // remove first char from left (startOfMultiDelim)
+        tempEndStrIndex = processedDelimStr.indexOf(endOfMultiDelim);
+        dynamicFirstMultiDelim = processedDelimStr.slice(0, tempEndStrIndex);
+        processedDelimStr = processedDelimStr.slice(tempEndStrIndex + 1);
+
+        // trying to find second multi char delimiter
+        if (processedDelimStr.indexOf(startOfMultiDelim) === 0) {
+          processedDelimStr = processedDelimStr.slice(1);
+          tempEndStrIndex = processedDelimStr.indexOf(endOfMultiDelim);
+          dynamicSecMultiDelim = processedDelimStr.slice(0, tempEndStrIndex);
+          processedDelimStr = processedDelimStr.slice(tempEndStrIndex + 1);
         }
-        return this.calcSum(arrayOfNumbers);
       }
 
-      // trying to find second multi char delimiter
-      if (inputString.indexOf(startOfMultiDelim) === 0) {
-        inputString = inputString.slice(1); // remove first char from left (startOfMultiDelim)
-        tempEndStringIndex = inputString.indexOf(endOfMultiDelim);
-        delim = inputString.slice(0, tempEndStringIndex);
-        inputString = inputString.slice(tempEndStringIndex + 1);
+      // finding single delimiter
+      else {
+        dynamicSingleDelim = processedDelimStr.slice(0, 1); // assigning first char after removed //
       }
-
-      // finding explicit delimiter
-      if (inputString.indexOf("\n") === 1) {
-        const rest = inputString.slice(2);
-        delim = inputString.slice(0, 1);
-        const array = rest.split(delim);
-
-        // convert array of strings to array of numbers
-        for (var i = 0; i < array.length; i++) {
-          arrayOfNumbers[i] = Number(array[i]);
-        }
-        //StringCalculator.convertToNumbers(array);
-        return this.calcSum(arrayOfNumbers);
-      }
-    } else {
-      if (inputString === "") {
-        return 0;
-      }
-      var values;
-      values = inputString.split(separator);
-      for (var i = 0; i < values.length; i++) {
-        arrayOfNumbers[i] = Number(values[i]);
-      }
-      return this.calcSum(arrayOfNumbers);
     }
-  }
 
-  static calcSum(arrayOfNumbers: number[]): number {
-    var sum = 0;
+    processedDelimStr = processedDelimStr.split(staticCommaDelim).join(" ");
+    processedDelimStr = processedDelimStr.split(staticNewLineDelim).join(" ");
+
+    if (dynamicSingleDelim !== "") {
+      processedDelimStr = processedDelimStr.split(dynamicSingleDelim).join(" ");
+    }
+
+    if (dynamicFirstMultiDelim !== "") {
+      // dynamic FIRST multi delimiter (not to process if not found in input)
+      processedDelimStr = processedDelimStr
+        .split(dynamicFirstMultiDelim)
+        .join(" "); // replace ALL MULTI dynamicFirstMultiDelim to spaces
+    }
+
+    if (dynamicSecMultiDelim !== "") {
+      // dynamic SECOND multi delimiter (not to process if not found in input)
+      processedDelimStr = processedDelimStr
+        .split(dynamicSecMultiDelim)
+        .join(" "); // replace ALL MULTI dynamicSecMultiDelim to spaces
+    }
+
+    arrayNumbersForSum = processedDelimStr.split(" ");
+
+    // convert array of strings to array of numbers
+    for (var i = 0; i < arrayNumbersForSum.length; i++) {
+      arrayNumbers[i] = Number(arrayNumbersForSum[i]);
+    }
+
     var invalidValues = [];
-    var separator = /,|\n/;
-    var values = arrayOfNumbers;
-    for (let value of values) {
+
+    if (inputString === "") {
+      return 0;
+    }
+    for (let value of arrayNumbers) {
       if (value < 0) {
         invalidValues.push(value);
-      } else if (separator) {
-        sum += value > 1000 ? 0 : value;
-      } else {
-        sum += value > 1000 ? 0 : value;
       }
     }
+
     if (invalidValues.length) {
+      return 0;
       throw new Error(
         "No negative values are allowed: " + invalidValues.join(",")
       );
     }
 
-    console.log("calculated sum: ", sum);
-    return sum || 0;
+    return StringCalculator.calcSum(arrayNumbers);
+  } // static add(inputString: string): number
+
+  static calcSum(arrayNumbersbers: number[]): number {
+    var sum = 0;
+    for (var i = 0; i < arrayNumbersbers.length; i++) {
+      sum += arrayNumbersbers[i] > 1000 ? 0 : arrayNumbersbers[i];
+    }
+    return sum;
   }
-}
+} // export class StringCalculator
